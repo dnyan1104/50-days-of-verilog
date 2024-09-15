b4f3b16aa9176443f7ca939d5374acc777c5720f
Dnyanraj Kekade <136449978+dnyan1104@users.noreply.github.com>
2024-09-15 15:44:03 +0530

    Create tb_Full_adder.v

---
+++ Day_2/tb_Full_adder.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.09.2024 15:39:04
+// Design Name: 
+// Module Name: tb_full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns/1ps
+module tb_Full_adder;
+    reg A, B, Cin;
+    wire Sum, Cout;
+
+    Full_adder uut (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
+
+    initial begin
+        $monitor("A = %b, B = %b, Cin = %b -> Sum = %b, Cout = %b", A, B, Cin, Sum, Cout);
+
+        A = 0; B = 0; Cin = 0; #10;
+        A = 0; B = 0; Cin = 1; #10;
+        A = 0; B = 1; Cin = 0; #10;
+        A = 0; B = 1; Cin = 1; #10;
+        A = 1; B = 0; Cin = 0; #10;
+        A = 1; B = 0; Cin = 1; #10;
+        A = 1; B = 1; Cin = 0; #10;
+        A = 1; B = 1; Cin = 1; #10;
+
+        $finish;
+    end
+endmodule
+
