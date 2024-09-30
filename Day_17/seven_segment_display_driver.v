586e87178da1e9b7a5e433c2b2e87ccb378a287f
Dnyanraj Kekade <136449978+dnyan1104@users.noreply.github.com>
2024-09-30 22:09:57 +0530

    Create seven_segment_display_driver.v

---
+++ Day_17/seven_segment_display_driver.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+module display_driver(
+input [3:0]bin,
+output reg [6:0]seg
+);
+always @ (*)begin
+case (bin)
+            4'b0000: seg = 7'b0111111;
+            4'b0001: seg = 7'b0000110; 
+            4'b0010: seg = 7'b1011011; 
+            4'b0011: seg = 7'b1001111; 
+            4'b0100: seg = 7'b1100110; 
+            4'b0101: seg = 7'b1101101; 
+            4'b0110: seg = 7'b1111101; 
+            4'b0111: seg = 7'b0000111; 
+            4'b1000: seg = 7'b1111111; 
+            4'b1001: seg = 7'b1101111; 
+            default: seg = 7'b0000000; 
+endcase
+end
+endmodule
