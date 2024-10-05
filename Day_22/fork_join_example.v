1b38b3c2a2cfefc448bf6a6aacf3ca1d263f6f6d
Dnyanraj Kekade <136449978+dnyan1104@users.noreply.github.com>
2024-10-05 19:19:24 +0530

    Update fork_join_example.v

--- Day_22/fork_join_example.v
+++ Day_22/fork_join_example.v
@@ -1,30 +1,21 @@
 `timescale 1ns / 1ps
 
-module fork_join_example;
+module counter_fork_join(
+    input clk,
+    input reset,
+    output reg [3:0] count
+);
 
-    reg a, b, c;
-    reg [1:0] result;
-
-    initial begin
-        a = 0;
-        b = 0;
-        c = 0;
-        result = 2'b00;
-
-        fork
-            begin
-                #10 a = 1;
-            end
-            begin
-                #15 b = 1;
-            end
-            begin
-                #20 c = 1;
-            end
-        join
-
-        result = {b, a};
-        #10 $finish;
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 4'b0000;
+        else begin
+            fork
+                begin
+                    count <= count + 1;
+                end
+            join
+        end
     end
 
 endmodule
