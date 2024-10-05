b07b3d178a7230bc0d935ef52fbdfdb3234c30fe
Dnyanraj Kekade <136449978+dnyan1104@users.noreply.github.com>
2024-10-05 19:20:53 +0530

    Update fork_join_example_tb.v

--- Day_22/fork_join_example_tb.v
+++ Day_22/fork_join_example_tb.v
@@ -6,31 +6,30 @@ module counter_fork_join_tb;
     reg reset;
     wire [3:0] count;
 
-    // Instantiate the module
+    
     counter_fork_join uut (
         .clk(clk),
         .reset(reset),
         .count(count)
     );
 
-    // Clock generation
+   
     initial begin
         clk = 0;
-        forever #5 clk = ~clk;  // 10 ns clock period (100 MHz)
+        forever #5 clk = ~clk;  
     end
 
-    // Test stimulus
+   
     initial begin
-        reset = 1;  // Apply reset
-        #10 reset = 0;  // Release reset after 10 ns
+        reset = 1; 
+        #10 reset = 0; 
+        #100 reset = 1;  
+        #10 reset = 0;  
 
-        #100 reset = 1;  // Reset the counter after 100 ns
-        #10 reset = 0;  // Release reset again
-
-        #200 $stop;  // End the simulation after 200 ns
+        #200 $stop; 
     end
 
-    // Monitor the output
+   
     initial begin
         $monitor("Time: %0t | Reset: %b | Count: %b", $time, reset, count);
     end
