25295445b7213a50279c0f3e0dd8babcc9cc7111
Dnyanraj Kekade <136449978+dnyan1104@users.noreply.github.com>
2024-10-21 16:18:42 +0530

    Create siso_shift_register.v

---
+++ Day_26/siso_shift_register.v
@@ -0,0 +1,19 @@
+module siso_shift_register (
+    input wire clk,
+    input wire rst,
+    input wire serial_in,
+    output wire serial_out
+);
+    reg [7:0] shift_reg;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            shift_reg <= 8'b0;
+        end else begin
+            shift_reg <= {shift_reg[6:0], serial_in};
+        end
+    end
+
+    assign serial_out = shift_reg[7];
+
+endmodule
